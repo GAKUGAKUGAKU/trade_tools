cc70526f5beba4867c710ac4c532b11e4af18e18
gaku <gaku5946@gmail.com>
2021-07-04 04:55:44 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ constapp = new Vue({
       strategy: '♂',
       ticker: '8',
       id: 'キジトラ',
-      favorite: '犬の人形',
+      action: '犬の人形',
       value: '1,000',
       quantity: 'eidi',
       cost: 'A',
@@ -22,7 +22,7 @@ constapp = new Vue({
       strategy: '♂',
       ticker: '7',
       id: '長毛種',
-      favorite: '人間',
+      action: '人間',
       value: '1,000',
       quantity: 'eidi',
       cost: 'A',
@@ -34,7 +34,7 @@ constapp = new Vue({
       strategy: '♀',
       ticker: '6',
       id: 'ミケ',
-      favorite: '高級ウェットフード',
+      action: '高級ウェットフード',
       value: '1,000',
       quantity: 'eidi',
       cost: 'A',
@@ -46,7 +46,7 @@ constapp = new Vue({
       strategy: '♀',
       ticker: '4',
       id: 'サビ',
-      favorite: '横取りフード',
+      action: '横取りフード',
       value: '1,000',
       quantity: 'eidi',
       cost: 'A',
@@ -64,7 +64,7 @@ constapp = new Vue({
         strategy: '♀',
         ticker: '4',
         id: 'サビ',
-        favorite: '横取りフード',
+        action: '横取りフード',
         value: '1,000',
         quantity: 'eidi',
         cost: 'A',
@@ -74,16 +74,17 @@ constapp = new Vue({
     },
     mod: function (event) {
       console.log("modButton click");
-      this.items.find((item) => item.name === "こなつ").favorite = "高いところ";
+      this.items.find((item) => item.no === "4").action = "更新された！！";
     },
     del: function (index) {
       console.log("dlelButton click");
-      this.items.splice(index,1);
+      this.items.splice(index, 1);
     },
     downloadcsv: function () {
+      console.log("downloadcsv click");
       var csv = '\uFEFF' + '#,日時,戦略,ティッカー,ID,買付/売却,価格,数量,手数料,理由,備考\n'
       this.items.forEach(el => {
-        var line = el['no'] + ',' + el['date'] + ',' +  el['strategy']  + ',' +  el['ticker'] + ','  +  el['id'] + ','  +  el['favorite'] + ','  +  el['value']  + ',' +  el['quantity'] + ','  +  el['cost'] + ','  +  el['reason']  + ',' +  el['memo'] + '\n'
+        var line = el['no'] + ',' + el['date'] + ',' + el['strategy'] + ',' + el['ticker'] + ',' + el['id'] + ',' + el['favorite'] + ',' + el['value'] + ',' + el['quantity'] + ',' + el['cost'] + ',' + el['reason'] + ',' + el['memo'] + '\n'
         csv += line
       })
       let blob = new Blob([csv], { type: 'text/csv' })
@@ -91,6 +92,51 @@ constapp = new Vue({
       link.href = window.URL.createObjectURL(blob)
       link.download = 'trade_data.csv'
       link.click()
+    },
+    save: function () {
+      console.log("save click");
+    },
+    analysis: function () {
+      console.log("save click");
+    },
+    uploadcsv: function (csvfile) {
+      console.log("uploadcsv click");
+      const file = csvfile.target.files[0];
+      const reader = new FileReader();
+      const workers = [];
+
+      const loadFunc = () => {
+        const lines = reader.result.split("\n");
+        lines.forEach(element => {
+          const workerData = element.split(",");
+          if (workerData.length != 11) return;
+          const worker = {
+            no: workerData[0],
+            date: workerData[1],
+            strategy: workerData[2],
+            ticker: workerData[3],
+            id: workerData[4],
+            action: workerData[5],
+            value: workerData[6],
+            quantity: workerData[7],
+            cost: workerData[8],
+            reason: workerData[9],
+            memo: workerData[10]
+          };
+          workers.push(worker);
+        });
+        this.workers = (workers);
+        this.workers.shift();
+        this.items = new Array();
+        this.items = this.items.concat(this.workers);
+      };
+      reader.onload = loadFunc;
+      reader.readAsText(file);
+
+      fetch('https://gakugakugaku.github.io/trade_tools/tradedata/trade_data.csv')
+        .then(response => response.text())
+        .then(data => console.log(data));
+      // .then(this.items.find((item) => item.no === "1").action = "a");
     }
   }
 }
